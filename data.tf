209c2cccfe51ac8f07d423a8a858ebd48626b178
srujanakamarsu <srujanamythree@gmail.com>
2023-08-22 12:26:05 +0530

    new commit

---
+++ data.tf
@@ -0,0 +1,8 @@
+data "aws_ssm_parameter" "master_username" {
+  name = "docdb.${var.env}.master_username"
+}
+
+
+data "aws_ssm_parameter" "master_password" {
+  name = "docdb.${var.env}.master_password"
+}
